0afbd615baa3c4d694cc8e215bb0391c7669ccc7
Sh3lan-12 <68787605+Sh3lan-12@users.noreply.github.com>
2022-12-29 23:26:26 +0200

    Add files via upload

---
+++ prime numbers/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿ Console.Write("Enter the starting number: ");
+ int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the ending number: ");
+ int n2 = int.Parse(Console.ReadLine());
+
+
+for (int i = n1; i <= n2; i++)
+{
+    if (IsPrime(i))
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+static bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    for (int i = 2; i <= Math.Sqrt(n); i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
